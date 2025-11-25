23348c76fd2ba84684d53bad578d68af6d5a66f9
Ichiyou1922 <mogi2fruits.kazu@gmail.com>
2025-11-26 00:40:51 +0900

    change my_cat codes

--- C_Practice/my_cat/main.c
+++ C_Practice/my_cat/main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+#define BUFFER_SIZE 256
 
-int main(void) {
-  FILE *fp = fopen("filename.txt", "r");
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Write your filename!");
+    return 1;
+  }
+
+  FILE *fp = fopen(argv[1], "r");
 
   if (fp == NULL){
-    fprintf(stderr, "Can`t open the filename.txt");
+    fprintf(stderr, "Can`t open file: %s\n", argv[1]);
     return 1;
   }
   
-  char buf[256];
+  char buf[BUFFER_SIZE];
   int line_num = 1;
   while(fgets(buf, sizeof(buf), fp) != NULL){
     printf("%d: %s", line_num, buf);
     line_num++;
   }
+  
   fclose(fp);
-
   return 0;
 }
